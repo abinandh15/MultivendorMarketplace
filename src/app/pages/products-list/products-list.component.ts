e7b8dd5f0856bcb4d4038c7e0efb444707640419
abinandh15 <abinandh15@gmail.com>
2020-02-26 23:41:41 +0530

    product component

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MediaObserver } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-products-list',
@@ -6,19 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products-list.component.sass']
 })
 export class ProductsListComponent implements OnInit {
-  products: { name: string, image: string, price: number }[];
+  products: { name: string; image: string; price: number }[];
 
-  constructor() { }
+  constructor(public media: MediaObserver) {}
 
   ngOnInit(): void {
     this.products = [
-      { name: 'Product1', image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-3_large.png?format=jpg&quality=90&v=1530129341', price: 25 },
-      { name: 'Product2', image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-4_large.png?format=jpg&quality=90&v=1530129360', price: 254 },
-      { name: 'Product3', image: 'https://cdn.shopify.com/s/files/1/0169/2660/5412/collections/placeholder-images-collection-1_large_807560ab-9024-46ea-ab0a-bb49df2b3bb8_1200x1200.png?v=1551259616', price: 25 },
-      { name: 'Product3', image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-collection-6_large.png?format=jpg&quality=90&v=1530129217', price: 255 },
-      { name: 'Product3', image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-collection-5_large.png?format=jpg&quality=90&v=1530129199', price: 525 },
-      { name: 'Product3', image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-2_large.png?format=jpg&quality=90&v=1530129318', price: 15 },
-    ]
+      {
+        name: 'Product1',
+        image:
+          'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-3_large.png?format=jpg&quality=90&v=1530129341',
+        price: 25
+      },
+      {
+        name: 'Product2',
+        image:
+          'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-4_large.png?format=jpg&quality=90&v=1530129360',
+        price: 254
+      },
+      {
+        name: 'Product3',
+        image:
+          // tslint:disable-next-line: max-line-length
+          'https://cdn.shopify.com/s/files/1/0169/2660/5412/collections/placeholder-images-collection-1_large_807560ab-9024-46ea-ab0a-bb49df2b3bb8_1200x1200.png?v=1551259616',
+        price: 25
+      },
+      {
+        name: 'Product3',
+        image:
+          'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-collection-6_large.png?format=jpg&quality=90&v=1530129217',
+        price: 255
+      },
+      {
+        name: 'Product3',
+        image:
+          'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-collection-5_large.png?format=jpg&quality=90&v=1530129199',
+        price: 525
+      },
+      {
+        name: 'Product3',
+        image:
+          'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-2_large.png?format=jpg&quality=90&v=1530129318',
+        price: 15
+      }
+    ];
   }
- 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,36 +2,36 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 // Angular Material Components
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatListModule} from '@angular/material/list';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatCardModule} from '@angular/material/card';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatIconModule} from '@angular/material/icon';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatTableModule} from '@angular/material/table';
-import {MatSortModule} from '@angular/material/sort';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { ChartsModule } from 'ng2-charts';
 import { NotFoundComponent } from './component/not-found/not-found.component';
 
@@ -41,11 +41,11 @@ import { NotFoundComponent } from './component/not-found/not-found.component';
   declarations: [NotFoundComponent],
   imports: [
     CommonModule,
-    FlexLayoutModule, 
-    ChartsModule
+    FlexLayoutModule,
+    ChartsModule,
   ],
   exports: [
-    FlexLayoutModule, 
+    FlexLayoutModule,
     MatCheckboxModule,
     MatCheckboxModule,
     MatButtonModule,
@@ -78,6 +78,8 @@ import { NotFoundComponent } from './component/not-found/not-found.component';
     MatSortModule,
     MatPaginatorModule,
     ChartsModule,
-    NotFoundComponent]
+    NotFoundComponent
+  ]
+
 })
-export class SharedModule { }
+export class SharedModule {}
